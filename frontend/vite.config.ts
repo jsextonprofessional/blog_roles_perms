b87bbb7f95dd46aa122abd4cf54ce64931f2629e
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-01-07 08:04:05 -0600

    [frontend] Removes /auth/ from path rewrite in vite.config.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 			'/api/': {
 				target: 'http://localhost:4000/',
 				changeOrigin: true,
-				rewrite: (path) => path.replace(/^\/api\/auth/, '')
+				rewrite: (path) => path.replace(/^\/api/, '')
 			}
 		}
 	}
