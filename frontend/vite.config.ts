c2c6df98bcd5150d15af00c2d9a985d8e992c738
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-01-07 09:30:08 -0600

    [frontend] Removes trailing slash from /api in proxy.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
 	server: {
 		proxy: {
-			'/api/': {
+			'/api': {
 				target: 'http://localhost:4000/',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
