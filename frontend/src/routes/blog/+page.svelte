a042fabddfe395aa3205c3e961087122309528c8
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-01-08 09:52:59 -0600

    [frontend] Creates routes/blog directory. Adds Blog and Comment components. Renders blog posts and comments.

---
+++ frontend/src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import BlogBody from './BlogBody.svelte';
+	import { blogPosts, comments } from '$lib/dummyData';
+</script>
+
+{#each blogPosts as post}
+	<BlogBody
+		title={post.title}
+		body={post.body}
+		comments={comments.filter((comment) => comment.postId === post.id)}
+	/>
+{/each}
---
+++ frontend/src/routes/blog/CommentBody.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	let { id, postId, authorId, body, createdAt, updatedAt } = $props();
+</script>
+
+<div class="comment-body my-2 rounded-md border-2 border-blue-500 p-4">
+	<p>{body}</p>
+	<p>Posted on {new Date(createdAt).toLocaleDateString()}</p>
+</div>
