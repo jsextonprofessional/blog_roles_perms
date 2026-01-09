d586ef139fc08c9891803b62b537b9009ec98412
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-01-09 06:18:02 -0600

    [frontend] Creates and mounts BlogTextArea component.

--- frontend/src/routes/blog/+page.svelte
+++ frontend/src/routes/blog/+page.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import BlogBody from './BlogBody.svelte';
 	import { blogPosts, comments } from '$lib/dummyData';
+	import BlogTextarea from './BlogTextarea.svelte';
 </script>
 
-{#each blogPosts as post}
-	<BlogBody
-		title={post.title}
-		body={post.body}
-		comments={comments.filter((comment) => comment.postId === post.id)}
-	/>
-{/each}
+<div class="flex flex-row border-2 border-yellow-500">
+	<div class="flex basis-2/3 flex-col">
+		{#each blogPosts as post}
+			<BlogBody
+				title={post.title}
+				body={post.body}
+				comments={comments.filter((comment) => comment.postId === post.id)}
+			/>
+		{/each}
+	</div>
+	<div class="flex basis-1/3 flex-col">
+		<BlogTextarea body="Write your blog post here..." />
+	</div>
+</div>
