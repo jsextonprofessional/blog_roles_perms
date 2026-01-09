71ed070d19edafb9badef95444ea17f153ff2a6f
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-01-09 07:47:32 -0600

    [frontend] Refactors Button component.

---
+++ frontend/src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let { type, label, formAction } = $props();
+</script>
+
+<button
+	{type}
+	formaction={formAction}
+	class="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+>
+	{label}
+</button>
