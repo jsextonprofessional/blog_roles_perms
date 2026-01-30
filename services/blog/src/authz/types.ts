c1ce6217c94ca1a7159acae02d9fb72e1e16d3ad
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-01-30 07:51:47 -0600

    [authz] Creates policy matrices and shared types.

--- services/blog/src/authz/types.ts
+++ services/blog/src/authz/types.ts
@@ -6,3 +6,13 @@ export type UserContext = {
 export type OwnableResource = {
   authorId: string;
 };
+
+export type Actor = "owner" | "otherUser" | "admin" | "unauthenticated";
+
+export type Policy = "create" | "edit" | "delete";
+
+export type PolicyMatrix = {
+  [P in Policy]: {
+    [A in Actor]: boolean;
+  };
+};
