8d3affcb440e45917a22a12b2f6bad9aaed486c9
Jacob Sexton <jsextonprofessional@protonmail.com>
2026-02-10 10:34:48 -0600

    [authn] Adds requireRole to admin-only route.

--- services/authn/routes/users.routes.ts
+++ services/authn/routes/users.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import * as UsersController from "../controllers/users.controller.js";
-import { authenticate } from "../middleware/auth.middleware.js";
+import { authenticate, requireRole } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", UsersController.registerUser);
 router.post("/login", UsersController.loginUser);
 router.get("/me", authenticate, UsersController.getCurrentUser);
-router.get("/admin-only", authenticate, UsersController.getAdminOnly);
+router.get(
+  "/admin-only",
+  authenticate,
+  requireRole("ADMIN"),
+  UsersController.getAdminOnly,
+);
 
 export default router;
